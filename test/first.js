f2bfebeb9b1d11720064583726af5939e0ca1790
Prasanth B <PrasanthB@quinnox.com>
2016-06-17 20:47:32 +0530

    -

--- test/first.js
+++ test/first.js
@@ -1,5 +1,12 @@
-describe('Name of the group', function() {
+describe('First test group', function() {
 	it('should add numbers', function(){
-		expect(1+2).toBe(4);
+		expect(2+2).toBe(4);
+		expect(1+2).toBe(3);
 	});
+
+	it('should subtract numbers', function(){
+		expect(1-2).toBe(-1);
+		expect(3-2).toBe(1);
+	});
+
 });
